0a85d79e45655ec350ce1e8ed559c6b899df1015
bundlebooth <hello@bundlebooth.ca>
2025-07-09 01:39:19 -0400

    Update backend.js

--- backend.js
+++ backend.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const fetch = require('node-fetch'); // Add this
 const app = express();
 
 app.use(cors());
